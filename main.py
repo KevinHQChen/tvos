ddfa644d1de171047f96743f815dbc5a6514d526
KHQC <khqc@khqc.ca>
2021-04-26 05:05:36 -0400

    changed CUDA GPU numbering

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from lib.utils import AverageMeter, rgb2class, setup_logger
 
 SCALE = 0.125
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '4,5,6,7'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 def parse_options():
     parser = argparse.ArgumentParser()
     parser.add_argument('--frame_num', '-n', type=int, default=10,
